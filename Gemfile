667a5a0f8be6599e042fb5205a3984f8588d63cf
Tea <tdinhla@protonmail.com>
2018-10-18 17:43:38 -0700

    use gem seed_dump to populate seeds.rb file

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'seed_dump'
+
 gem 'faker'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-3.times do
-  product = Product.new(name: Faker::Commerce.product_name, price: Faker::Commerce.price, image_url: "some_image.jpeg", description: Faker::Commerce.material)
-
-  product.save
-end
+Product.create!([
+  {name: "Leuchtturm1917 Hardcover Notebook - Medium (A5)", price: "20.0", image_url: "some-image.jpeg", description: "yellow covered notebook, dotted", stock_status: nil},
+  {name: "Leuchtturm1917 Hardcover Notebook - Small (A6)", price: "14.0", image_url: "some-image.jpeg", description: "berry covered notebook, blank", stock_status: nil},
+  {name: "Durable Granite Bottle", price: "70.0", image_url: "some_image.jpeg", description: "Copper", stock_status: nil},
+  {name: "Heavy Duty Paper Hat", price: "86.0", image_url: "some_image.jpeg", description: "Wool", stock_status: nil},
+  {name: "q", price: "1.0", image_url: "w", description: "e", stock_status: nil},
+  {name: "a", price: "2.0", image_url: "s", description: "d", stock_status: nil}
+])
